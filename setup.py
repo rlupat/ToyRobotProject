f520c2ddc0357dc8482d20c25c75a716ee7e0fad
Richard Lupat <rlupat@users.noreply.github.com>
2022-01-11 22:53:24 +1100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="rlupat.toyrobot",
+    version="0.1.1",
+    description="ToyRobot Example Implementation",
+    author="Richard Lupat",
+    license="MIT",
+    packages=["toyrobot"]
+    + ["toyrobot." + p for p in sorted(find_packages("./toyrobot"))],
+    install_requires=[
+        "wheel"
+    ],
+    entry_points={"console_scripts": ["toyrobot=toyrobot.app:main"]},
+    zip_safe=False,
+    long_description="ToyRobot Example Implementation",
+    long_description_content_type="text/markdown",
+)
