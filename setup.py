cc90e5af16ae8cb86f16d824ab9bf44328ba94de
Richard Lupat <rlupat@users.noreply.github.com>
2025-08-15 14:22:23 +1000

    Update setup.py version to v0.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="rlupat.toyrobot",
-    version="0.2.2",
+    version="0.2.3",
     description="ToyRobot Example Implementation",
     author="Richard Lupat",
     license="MIT",
